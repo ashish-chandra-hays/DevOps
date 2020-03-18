5392b07bc11a554885f5ae081726641c455bd2ed
Chandra <Ashish.Chandra@hays.com>
2020-03-18 14:27:20 +0530

    deleting springboot liquibase project

--- src/main/java/com/liquibasePoc/persistence/repositories/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.liquibasePoc.persistence.repositories;
-
-import com.liquibasePoc.persistence.entities.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Integer> {
-
-}
